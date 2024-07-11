743974667b7e82214fd4b9bd25a39426922a9f53
Luis Donoso <ldonosoval2@gmail.com>
2024-07-11 16:08:14 -0400

    funcion de ocultar texto con un click en la imagen del card

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,3 +4,17 @@
           $("#carouselExamplelidesOnly").fadeOut();
         });
       });
+
+      //oculta texto cuan se da un click en la imagen
+      $("#image1").on( "click", function(){
+        $("#texto1").toggle();
+      });
+      $("#image2").on( "click", function(){
+        $("#texto2").toggle();
+      });
+      $("#image3").on( "click", function(){
+        $("#texto3").toggle();
+      });
+      $("#image4").on( "click", function(){
+        $("#texto4").toggle();
+      });
